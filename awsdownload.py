fbc8cfa7d8a40a2bce7ef7982e494952940764d8
Kody Moodley <kodymoodley@users.noreply.github.com>
2019-01-21 12:35:52 +0100

    Create awsdownload.py

---
+++ awsdownload.py
@@ -0,0 +1,10 @@
+import boto3
+
+BUCKET_NAME = 'lipchat-word-embeddings' # replace with your bucket name
+KEY = 'UEXescIMZh79tyCAMFUwr6tvH59VTrJkQvgpNrnR' # replace with your object key
+ID = 'AKIAJLHO7L2GJ3R4GLPQ'
+
+session = boto3.Session(region_name = 'eu-central-1', aws_access_key_id=ID, aws_secret_access_key=KEY)
+
+myclient = session.resource('s3')
+myclient.Bucket(BUCKET_NAME).download_file('GoogleNews-vectors-negative300.bin', 'model/GoogleNews-vectors-negative300.bin')
